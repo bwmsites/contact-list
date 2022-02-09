83d62f200dd35e0b07ed91373c3e0888762d31db
Bruno Silva <bwmsites@gmail.com>
2022-02-09 20:55:25 -0300

    adds styling package to project

--- contactor/src/App.js
+++ contactor/src/App.js
@@ -1,20 +1,11 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import Home from './pages/home'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Home />
+    </ChakraProvider>
   );
 }
 
