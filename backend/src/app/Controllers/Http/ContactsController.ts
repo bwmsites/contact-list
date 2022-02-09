4cdaed8a3dde43303fc698ca8275446a737adc71
Bruno Silva <bwmsites@gmail.com>
2022-02-09 20:00:38 -0300

    adds request payload validations to contacts controller

--- backend/src/app/Controllers/Http/ContactsController.ts
+++ backend/src/app/Controllers/Http/ContactsController.ts
@@ -1,89 +1,114 @@
+/* eslint-disable @typescript-eslint/explicit-member-accessibility */
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import Contact from 'App/Models/Contact'
 
 export default class ContactsController {
-    emailExistsResponse = {
-        message: 'The given email address already exists in database',
-        code: 'EMAIL_ALREADY_EXISTIS'
-    }
+	emailExistsResponse = {
+		message: 'The given email address already exists in database',
+		code: 'EMAIL_ALREADY_EXISTIS',
+	}
 
-    public async getAllContacts({ request }: HttpContextContract) {
-        const { includeDeleted } = request.qs()
-        
-        if (includeDeleted) {
-            return await Contact.all()
-        }
+	public async getAllContacts({ request }: HttpContextContract) {
+		const { includeDeleted } = request.qs()
 
-        return await Contact.findByOrFail('deleted', false)
-    }
+		if (includeDeleted) {
+			return await Contact.all()
+		}
 
-    public async getContactById({ request }: HttpContextContract) {
-        const contactId = Number(request.param('contactId'))
+		return await Contact.findByOrFail('deleted', false)
+	}
 
-        return await Contact.find(contactId)
-    }
+	public async getContactById({ request }: HttpContextContract) {
+		const contactId = Number(request.param('contactId'))
 
-    public async createContact({ request, response }: HttpContextContract) {
-        const { name, email } = request.body()
+		return await Contact.find(contactId)
+	}
 
-        try {
-            // Despite the database already handles duplicates I decided to use this approach for
-            // handling the response manually
-            const contact = await Contact.findBy('email', email)
+	public async createContact({ request, response }: HttpContextContract) {
+		const newContactSchema = schema.create({
+			name: schema.string(),
+			email: schema.string({}, [
+				rules.email({
+					sanitize: true,
+					ignoreMaxLength: true,
+					domainSpecificValidation: true,
+				}),
+			]),
+		})
 
-            if (contact) {
-                return response.status(422).send(this.emailExistsResponse)
-            }
+		try {
+			const { name, email } = await request.validate({ schema: newContactSchema })
 
-            const newContact = await Contact.create({ name, email, deleted: false });
-            return response.status(201).send(newContact)
-        } catch (error) {
-            throw new Error(error)
-        }
-    }
+			// Despite the database already handles duplicates I decided to use this approach for
+			// handling the response manually
+			const contact = await Contact.findBy('email', email)
 
-    public async updateContact({ request, response }: HttpContextContract) {
-        const contactId = Number(request.param('contactId'))
-        const { name, email } = request.only(['name', 'email'])
+			if (contact) {
+				return response.status(422).send(this.emailExistsResponse)
+			}
 
-        try {
-            // Despite the database already handles duplicates I decided to use this approach for
-            // handling the response manually and make it more friendly
-            if (email) {
-                const existingContact = await Contact.findBy('email', email)
+			const newContact = await Contact.create({ name, email, deleted: false })
+			return response.status(201).send(newContact)
+		} catch (error) {
+			response.badRequest(error.messages)
+		}
+	}
 
-                if (existingContact && existingContact.contactId !== contactId) {
-                    return response.status(422).send(this.emailExistsResponse)
-                }
-            }
+	public async updateContact({ request, response }: HttpContextContract) {
+		const contactId = Number(request.param('contactId'))
 
-            const contact = await Contact.findOrFail(contactId)
-            contact.merge({ name, email })
-            await contact.save()
-            
-            return contact
-        } catch (error) {
-            throw new Error(error)
-        }
-    }
+		const updateContactSchema = schema.create({
+			name: schema.string.optional(),
+			email: schema.string.optional({}, [
+				rules.email({
+					sanitize: true,
+					ignoreMaxLength: true,
+					domainSpecificValidation: true,
+				}),
+			]),
+		})
 
-    public async deleteContact({ request }: HttpContextContract) {
-        const contactId = Number(request.param('contactId'))
+		try {
+			const { name, email } = await request.validate({ schema: updateContactSchema })
 
-        const contact = await Contact.findOrFail(contactId)
-        contact.deleted = true
-        await contact.save()
+			// Despite the database already handles duplicates I decided to use this approach for
+			// handling the response manually and make it more friendly
+			if (email) {
+				const existingContact = await Contact.findBy('email', email)
 
-        return { id: contactId }
-    }
+				if (existingContact && existingContact.contactId !== contactId) {
+					return response.status(422).send(this.emailExistsResponse)
+				}
+			}
 
-    public async restoreContact({ request }: HttpContextContract) {
-        const contactId = Number(request.param('contactId'))
+			const contact = await Contact.findOrFail(contactId)
+			contact.merge({ name, email })
+			await contact.save()
 
-        const contact = await Contact.findOrFail(contactId)
-        contact.deleted = false
-        await contact.save()
+			return contact
+		} catch (error) {
+			response.badRequest(error.messages)
+		}
+	}
 
-        return contact
-    }
+	public async deleteContact({ request }: HttpContextContract) {
+		const contactId = Number(request.param('contactId'))
+
+		const contact = await Contact.findOrFail(contactId)
+		contact.deleted = true
+		await contact.save()
+
+		return { id: contactId }
+	}
+
+	public async restoreContact({ request }: HttpContextContract) {
+		const contactId = Number(request.param('contactId'))
+
+		const contact = await Contact.findOrFail(contactId)
+		contact.deleted = false
+		await contact.save()
+
+		return contact
+	}
 }
